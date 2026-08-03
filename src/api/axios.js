5be61d31ca617ffab5e7cf66b02f6feac38ac89a
Galaksi <gumus.huseynovatv2007@gmail.com>
2026-08-03 21:54:56 +0400

    yeto

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL:  import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,9 +9,18 @@ const apiClient = axios.create({
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("authToken");
+
+  console.log("========== REQUEST ==========");
+  console.log("REQUEST URL:", config.baseURL + config.url);
+  console.log("METHOD:", config.method);
+  console.log("REQUEST BODY:", config.data);
+  console.log("AUTH TOKEN:", token ? "Var" : "Yox");
+  console.log("=============================");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
