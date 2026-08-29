c32b3a96495d7c04615e9b0d745e65e7c9628639
shabising <shabismanova127@gmail.com>
2026-08-30 03:30:07 +0400

    FAQ səhifəsi yeniləndi

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -4,22 +4,13 @@ import { useLanguage } from '../hooks/useLanguage'
 
 
 const FAQ_KEYS = [
-  { qKey: 'faq_q1',  aKey: 'faq_a1' },
-  { qKey: 'faq_q2',  aKey: 'faq_a2' },
-  { qKey: 'faq_q3',  aKey: 'faq_a3' },
-  { qKey: 'faq_q4',  aKey: 'faq_a4' },
-  { qKey: 'faq_q5',  aKey: 'faq_a5' },
-  { qKey: 'faq_q6',  aKey: 'faq_a6' },
-  { qKey: 'faq_q7',  aKey: 'faq_a7' },
-  { qKey: 'faq_q8',  aKey: 'faq_a8' },
-  { qKey: 'faq_q9',  aKey: 'faq_a9' },
-  { qKey: 'faq_q10', aKey: 'faq_a10' },
-  { qKey: 'faq_q11', aKey: 'faq_a11' },
-  { qKey: 'faq_q12', aKey: 'faq_a12' },
-  { qKey: 'faq_q13', aKey: 'faq_a13' },
-  { qKey: 'faq_q14', aKey: 'faq_a14' },
-  { qKey: 'faq_q15', aKey: 'faq_a15' },
-  { qKey: 'faq_q16', aKey: 'faq_a16' },
+  { qKey: 'faq_q1', aKey: 'faq_a1' },
+  { qKey: 'faq_q2', aKey: 'faq_a2' },
+  { qKey: 'faq_q3', aKey: 'faq_a3' },
+  { qKey: 'faq_q4', aKey: 'faq_a4' },
+  { qKey: 'faq_q5', aKey: 'faq_a5' },
+  { qKey: 'faq_q6', aKey: 'faq_a6' },
+  { qKey: 'faq_q7', aKey: 'faq_a7' },
 ]
 
 function FAQItem({ question, answer, isOpen, onToggle, itemRef }) {
