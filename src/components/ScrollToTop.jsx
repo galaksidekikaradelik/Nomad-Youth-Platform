ab4a3ffdccf86953928c049444967c8a3cd27a8f
galaksidekikaradelik <gumus.huseynovatv2007@gmail.com>
2026-09-06 22:13:52 +0400

    new bug bug

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,11 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
-
 
 export default function ScrollToTop() {
   const { pathname } = useLocation()
+  const prevPathnameRef = useRef(pathname)
 
   useEffect(() => {
+    const prevPathname = prevPathnameRef.current
+    prevPathnameRef.current = pathname
+    const isOpportunitiesInternalTransition =
+      prevPathname.startsWith('/opportunities') &&
+      pathname.startsWith('/opportunities')
+
+    if (isOpportunitiesInternalTransition) {
+      return
+    }
+
     window.scrollTo(0, 0)
   }, [pathname])
 
